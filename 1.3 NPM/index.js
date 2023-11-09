b8821e1cf3ca56f63ed08406a9c23ac5012064bd
Pallav Goswami <66618685+pallavgoswami@users.noreply.github.com>
2023-11-10 01:54:58 +0530

    Add files via upload

---
+++ 1.3 NPM/index.js
@@ -0,0 +1,18 @@
+import generateName from "sillyname";
+var sillyName = generateName();
+console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes" // superheroes object from the package
+const name = superheroes.random();
+console.log(`My name is ${name}!`);
+
+// String interpolation allows injecting variables, function calls, arithmetic expressions directly into a string.
+// String Interpolation 
+// function myInfo(fname, lname, country) { 
+//     return `My name is ${fname} ${lname}. ${country} is my favorite country`;  
+// } 
+// console.log(myInfo("john", "doe", "India"));
+
+
+
+
